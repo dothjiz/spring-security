c64a5be8d97cde89e1928225d0357be56c4c83b5
dothjiz <dothjiz@gmail.com>
2020-04-09 10:21:58 +0200

    Added JWT functionality and security configuration

---
+++ src/main/java/com/doth/demo/security/SecurityConfigurer.java
@@ -0,0 +1,27 @@
+package com.doth.demo.security;
+
+import com.doth.demo.security.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService);
+    }
+
+    @Bean
+    public PasswordEncoder noPaaswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
